8c679442a282fd229fa0b03c51ccc7931f0d0f44
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-07 12:59:37 -0500

    redone for exam practice

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
-    # your code here
     if (username == "admin" or username == "ADMIN") and password == "12345":
         return "Access granted"
     else:
         return "Access denied"
 
-def hows_the_weather(temperature):
-    # your code here
-    if temperature < 40:
+
+def hows_the_weather(temp):
+    if temp < 40:
         return "It's brisk out there!"
-    elif temperature <= 65:
+    elif temp < 65:
         return "It's a little chilly out there!"
-    elif temperature > 85:
-        return "It's too dang hot out there!"
-    else:
+    elif temp <= 85:
         return "It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
+
 
 def fizzbuzz(num):
-    # your code here
-    if not num % 3 and not num % 5:
+    if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
-    elif not num % 3:
+    elif num % 3 == 0:
         return "Fizz"
-    elif not num % 5:
+    elif num % 5 == 0:
         return "Buzz"
     else:
         return num
 
+
 def calculator(operation, num1, num2):
-    # your code here
-    if operation == '+':
-        return num1 + num2
-    elif operation == '-':
-        return num1 - num2
-    elif operation == '*':
-        return num1 * num2
-    elif operation == '/':
-        return num1 / num2
+    calculations = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2,
+    }
+    if operation in calculations:
+        return calculations[operation]
     else:
         print("Invalid operation!")
         return None
